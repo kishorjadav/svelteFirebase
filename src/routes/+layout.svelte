fb646135a577ef2d8e96659a1d0eab5e28872cc4
Kishor Jadav <53091037+kishorjadav@users.noreply.github.com>
2023-05-20 13:39:00 +0530

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  //pacakage
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+
+  import { accessStore, userRefreshStore } from "../cookies/cookieStore";
+  import { authUserStore } from "../store";
+
+  import { auth } from "../lib/firebase/firebase.client";
+  import axios from "axios";
+
+  //components
+  import "../app.css";
+  import AuthSidebar from "../lib/AuthSidebar.svelte";
+  import AuthTopHeader from "../lib/AuthTopHeader.svelte";
+  import SignIn from "./signin/+page.svelte";
+
+  let slotWithToken = false;
+  let result: any;
+
+  let fields = {
+    email: "",
+    localId: "",
+    photo: "",
+  };
+
+  onMount(() => {
+    checkForAccess();
+    console.log("tokens--->", $accessStore, $userRefreshStore);
+  });
+
+  let checkForAccess = async () => {
+    // checking with store
+    if ($accessStore || $userRefreshStore) {
+      slotWithToken = true;
+      fetchData();
+    } else {
+      goto("/signin");
+    }
+  };
+
+  let fetchData = async () => {
+    try {
+      const res = await axios.post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDK0R6x-4ai29YULdMaXOL78DeJP5iPwtA",
+        {
+          idToken: $accessStore,
+        }
+      );
+      result = res.data.users;
+
+      fields.email = result.map((item) => item.email);
+      fields.localId = result.map((item) => item.localId);
+      authUserStore.update((curr) => {
+        return { ...curr, isLoading: false, currentUser: fields.localId };
+      });
+      console.log("fleids checking", $authUserStore);
+    } catch (e) {
+      // let error = e.response;
+      // if (error.status >= 400 && error.status <= 499) {
+      //   if (error.status === 403) {
+      //     hasPermission = false;
+      //   }
+      //   toast.error(
+      //     error.data.message || "Something wrong when fetching admin lists"
+      //   );
+      // }
+    }
+  };
+</script>
+
+{#if $accessStore && $userRefreshStore && slotWithToken}
+  <main>
+    <!-- desktop devices -->
+    <div class="overflow-hidden">
+      <section class="sr-only md:not-sr-only h-screen">
+        <AuthSidebar accessToComp={$accessStore}>
+          <div class="overflow-hidden">
+            <AuthTopHeader accessToComp={$accessStore} />
+          </div>
+          <div
+            class={` ${
+              $accessStore ? "  overflow-y-auto h-screen p-2" : ""
+            }   h-full `}
+          >
+            <slot />
+            {$authUserStore.currentUser}
+          </div>
+        </AuthSidebar>
+      </section>
+    </div>
+
+    <!-- mobile devices -->
+    <div class="md:sr-only not-sr-only">
+      <!-- <section>
+        <AuthSidebar accessToComp={$accessStore} />
+      </section> -->
+      <section class="overflow-hidden">
+        <AuthTopHeader accessToComp={$accessStore} />
+      </section>
+      <div
+        class={` ${$accessStore ? "  overflow-y-auto h-screen" : ""}   h-full `}
+      >
+        <slot />
+      </div>
+    </div>
+  </main>
+{:else}
+  <!-- <SignIn /> -->
+
+  <slot />
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { getCookie, removeCookie } from "../cookies/useCookies";
+  import {
+    AUTH_COOKIE_NAME,
+    AUTH_REFRESH_COOKIE_NAME,
+  } from "../cookies/cookieName";
+
+  let accessToken: any;
+  let refreshToken: any;
+
+  onMount(() => {
+    checkForAccess();
+  });
+
+  let checkForAccess = async () => {
+    accessToken = getCookie(AUTH_COOKIE_NAME);
+    refreshToken = getCookie(AUTH_REFRESH_COOKIE_NAME);
+  };
+</script>
+
+<section class="bg-red-200">
+  ihujk
+  <!-- <h1 class="text-red-500 text-xl">accessToken:{accessToken}</h1>
+  <h1 class="text-red-500 text-xl">refreshToken:{refreshToken}</h1> -->
+</section>
