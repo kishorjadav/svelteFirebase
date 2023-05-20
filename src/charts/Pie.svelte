70b6e400a0061a4798228b61ab8fe7c2a27422a9
kishorjadavb <kishore@digitalprizm.net>
2023-05-20 16:31:14 +0530

    uifiixed

---
+++ src/charts/Pie.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Pie } from "svelte-chartjs";
+  import { data } from "./line.js";
+
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+    CategoryScale,
+  } from "chart.js";
+
+  ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+</script>
+
+<Pie {data} options={{ responsive: true }} />
