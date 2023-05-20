175ce086bab08147f058e0df6dba4c1eaea1b559
Kishor Jadav <53091037+kishorjadav@users.noreply.github.com>
2023-05-20 13:44:30 +0530

    Add files via upload

---
+++ src/schemas/customValidation.ts
@@ -0,0 +1,62 @@
+import * as yup from 'yup';
+
+export const LoginSchema = yup.object().shape({
+    email: yup.string().email().label('Email').required(),
+    password: yup
+        .string()
+        .label('Password')
+        .min(8, 'Password must be 8 alphanumeric characters')
+        .matches(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+            'Must be atleast 1 uppercase, 1 lowercase & 1 numeric letter'
+        ).required(),
+    // confirm_password: yup
+    //     .string()
+    //     .label('confirm password')
+    //     .required()
+    //     .oneOf([yup.ref('password'), null], 'Passwords must match')
+});
+
+export const RegisterSchema = yup.object().shape({
+    email: yup.string().email().label('Email').required(),
+    password: yup
+        .string()
+        .label('Password')
+        .min(8, 'Password must be 8 alphanumeric characters')
+        .matches(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+            'Must be atleast 1 uppercase, 1 lowercase & 1 numeric letter'
+        ).required(),
+    confirmPassword: yup
+        .string()
+        .label('confirm password')
+        .required()
+        .oneOf([yup.ref('password'), null], 'Passwords must match')
+});
+
+export const forgotSchema = yup.object().shape({
+    email: yup.string().email().label('Email').required(),
+
+});
+
+
+export const ResetPasswordSchema = yup.object().shape({
+    password: yup
+        .string()
+        .label('Password')
+        .min(8, 'Password must be 8 alphanumeric characters')
+        .matches(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+            'Must be atleast 1 uppercase, 1 lowercase & 1 numeric letter'
+        ).required(),
+    confirmPassword: yup
+        .string()
+        .label('confirm password')
+        .required()
+        .oneOf([yup.ref('password'), null], 'Passwords must match')
+});
+
+export const customerSchema = yup.object().shape({
+    email: yup.string().email().label('Email').required(),
+    firstName: yup.string().label('First Name').required(),
+});
